f59cf37b003c48e198862450c6729d77b390ae66
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-08-25 09:20:42 -0400

    fetch_tags script

--- fetch_tags.sh
+++ fetch_tags.sh
@@ -20,9 +20,12 @@ NEW_FILENAME=$NAME.new
 NEW_FILEPATH=$DATA_DIR/$NEW_FILENAME
 LATEST_TAG="latest"     # dummy one, to get replaced during API call. Needs to be more specific than 'latest'
 
+TAG_INDEX=0
+
 while [ "$LATEST_TAG" == "latest" ]
-do 
-    LATEST_TAG=$(wget -q https://registry.hub.docker.com/v2/repositories/$DOCKER_HUB_REPO/tags -O- | jq '.results[0].name' | tr -d \")
+do
+    LATEST_TAG=$(wget -q https://registry.hub.docker.com/v2/repositories/$DOCKER_HUB_REPO/tags -O- | jq '.results['$TAG_INDEX'].name' | tr -d \")
+    TAG_INDEX=$TAG_INDEX+1
 done
 
 if [ "$LATEST_TAG" == "" ]; then
@@ -40,15 +43,18 @@ if [ ! -f "$NEW_FILEPATH" ]; then
 fi
 
 
-# diff
+# diff with old file
 OLD_FILENAME=$NAME.old
 OLD_FILEPATH=$DATA_DIR/$OLD_FILENAME
 
 if [ -f "$OLD_FILEPATH" ] && [ -f "$NEW_FILEPATH" ]; then
-    diff $OLD_FILEPATH $NEW_FILEPATH
+    DIFF=$(diff $OLD_FILEPATH $NEW_FILEPATH)
 
-    # get the latest tag ()
-
+    if [ "$DIFF"  == "" ]; then
+        echo "[INFO] No new tag found. Exiting"
+        rm $NEW_FILEPATH
+        exit 0
+    fi
 
     # rotate historical files
     rm $OLD_FILEPATH
