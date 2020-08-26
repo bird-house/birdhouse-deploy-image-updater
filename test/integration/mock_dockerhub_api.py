c0250c2801e6a5d7facc294a5d0657a7badd6692
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-08-26 16:47:17 -0400

    api mock impl

--- test/integration/mock_dockerhub_api.py
+++ test/integration/mock_dockerhub_api.py
@@ -7,20 +7,31 @@ app = Flask(__name__)
 # testing paths:
 # curl 192.168.99.201:5000/v2/repositories/birdhouse/finch/tags
 # curl 192.168.99.201:5000/v2/repositories/pavics/weaver/tags
+# curl -XPOST localhost:5000/birdhouse/finch/0.5.4
 
+
+BIRDHOUSE_FINCH_DATA = json.load(open("./mock_data/birdhouse_finch"))
+PAVICS_WEAVER_DATA = json.load(open("./mock_data/pavics_weaver"))
+
+dockerhub_mock_data = {
+    "birdhouse_finch" : BIRDHOUSE_FINCH_DATA,
+    "pavics_weaver" : PAVICS_WEAVER_DATA
+}
 
 
 @app.route('/v2/repositories/<dockerhub_project>/<dockerhub_repo>/tags', methods=['GET'])
 def get_tags(dockerhub_project, dockerhub_repo):
-    filepath = "./mock_data" + "/" + dockerhub_project + "_" + dockerhub_repo
-
-    if path.exists(filepath):
-        f = open(filepath)
-        data = json.load(f)
-    else:
-        abort(404)
+    data_key = dockerhub_project + "_" + dockerhub_repo
+    data = dockerhub_mock_data[data_key]
     
     return data
+
+
+@app.route('/<dockerhub_project>/<dockerhub_repo>/<tagname>', methods=['POST'])
+def post_tag(dockerhub_project, dockerhub_repo, tagname):
+    data_key = dockerhub_project + "_" + dockerhub_repo
+
+    return "added tag " + tagname + " for " + dockerhub_project + "/" + dockerhub_repo
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
