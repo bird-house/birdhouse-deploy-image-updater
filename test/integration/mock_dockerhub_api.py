60a77434e7cc4a7d963a9091cd71d50e41db60e5
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-08-26 15:46:32 -0400

    api mock

--- test/integration/mock_dockerhub_api.py
+++ test/integration/mock_dockerhub_api.py
@@ -8,14 +8,16 @@ BIRDHOUSE_FINCH_MOCK="mock_data/birdhouse_finch"
 PAVICS_WEAVER_MOCK="mock_data/pavics_weaver"
 
 
-@app.route('/v2/repositories/<dockerhub_repo>/tags', methods=['POST'])
-def get_tags(dockerhub_repo):
+@app.route('/v2/repositories/<dockerhub_project>/<dockerhub_repo>/tags', methods=['GET'])
+def get_tags(dockerhub_project, dockerhub_repo):
+    print(dockerhub_project)
     print(dockerhub_repo)
 
+    filepath = "mock_data" + "/" + dockerhub_project + "_" + dockerhub_repo
 
-    data = json.load(json_file)
+    data = json.load(filepath)
     
-    return "ok"
+    return data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
