7251485b214eee925a4312a0a8457df4ac20578b
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-08-27 08:08:42 -0400

    updated integration test report

--- test/integration/integration_test_runner.sh
+++ test/integration/integration_test_runner.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 # free port for dummy API
 lsof -ti tcp:5000 | xargs kill &> /dev/null
 
@@ -6,7 +12,9 @@ printf "%s\n" "" "    [TEST] Starting dummy APIs" ""
 python3 mock_dockerhub_api.py &> /dev/null  &
 echo "done"
 
+# cd to main dir and clean historic tag data
 cd ../..
+rm -rf data/
 
 # run updater CLI
 printf "%s\n" "" "    [TEST] Running updater - no PR to create" ""
@@ -26,11 +34,15 @@ source test/integration/env.test && ./main.sh
 ### Asserts that diff contains the right thing
 printf "%s\n" "" "    [TEST] ASSERT" ""
 if grep -q 'export FINCH_IMAGE="pavics/weaver:1.13.2-worker"' "last-diff-result.log"; then
-    echo "[INFO] [bump_weaver-worker_to_1.13.2-worker] The commit content looks good"
-    # echo
-    # cat last-diff-result.log
+    printf "${GREEN}[INFO] [bump_weaver-worker_to_1.13.2-worker] The commit content looks good"
+    echo
+    cat last-diff-result.log
+    printf "${NC}"
 else
-    echo "ERROR - wrong commit content. Exiting."
+    printf "${RED}[ERROR] [bump_weaver-worker_to_1.13.2-worker] wrong commit content. Exiting."
+    echo
+    cat last-diff-result.log
+    printf "${NC}"
     exit 1
 fi
 
@@ -43,6 +55,8 @@ source test/integration/env.test && ./main.sh
 # update an image
 printf "%s\n" "" "    [TEST] Pushing weaver and finch tags to DockerHub" ""
 curl -XPOST localhost:5000/pavics/weaver/1.13.3-worker
+curl -XPOST localhost:5000/pavics/weaver/1.13.3-manager
+curl -XPOST localhost:5000/pavics/weaver/1.13.3
 curl -XPOST localhost:5000/birdhouse/finch/version-0.5.4
 
 
@@ -55,12 +69,15 @@ source test/integration/env.test && ./main.sh
 ### Asserts that diff contains the right thing
 printf "%s\n" "" "    [TEST] ASSERT" ""
 if grep -q 'export FINCH_IMAGE="birdhouse/finch:version-0.5.4"' "last-diff-result.log"; then
-    echo "[INFO] [bump_finch_to_version-0.5.4] The commit content looks good"
-    # echo
-    # cat last-diff-result.log
-else
-    echo "ERROR - wrong commit content. Exiting."
+    printf "${GREEN}[INFO] [bump_finch_to_version-0.5.4] The commit content looks good"
+    echo
     cat last-diff-result.log
+    printf "${NC}"
+else
+    printf "${RED}[ERROR] [bump_finch_to_version-0.5.4] wrong commit content. Exiting."
+    echo
+    cat last-diff-result.log
+    printf "${NC}"
     exit 1
 fi
 
@@ -74,11 +91,15 @@ source test/integration/env.test && ./main.sh
 ### Asserts that diff contains the right thing
 printf "%s\n" "" "    [TEST] ASSERT" ""
 if grep -q 'export FINCH_IMAGE="pavics/weaver:1.13.3-worker"' "last-diff-result.log"; then
-    echo "[INFO] [bump_weaver-worker_to_1.13.3-worker] The commit content looks good"
-    # echo
-    # cat last-diff-result.log
+    printf "${GREEN}[INFO] [bump_weaver-worker_to_1.13.3-worker] The commit content looks good"
+    echo
+    cat last-diff-result.log
+    printf "${NC}"
 else
-    echo "ERROR - wrong commit content. Exiting."
+    printf "${RED}[ERROR] [bump_weaver-worker_to_1.13.3-worker] wrong commit content. Exiting."
+    echo
+    cat last-diff-result.log
+    printf "${NC}"
     exit 1
 fi
 
