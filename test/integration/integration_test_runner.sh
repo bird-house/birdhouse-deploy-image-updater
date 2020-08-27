91e84a688ed29e5ea4418791952cc82dc7becd3a
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-08-27 07:52:22 -0400

    added test in integration_test_runner

--- test/integration/integration_test_runner.sh
+++ test/integration/integration_test_runner.sh
@@ -7,23 +7,81 @@ python3 mock_dockerhub_api.py &> /dev/null  &
 echo "done"
 
 cd ../..
-rm last-diff-result.log
 
 # run updater CLI
 printf "%s\n" "" "    [TEST] Running updater - no PR to create" ""
+rm -f last-diff-result.log
 source test/integration/env.test && ONLY_UPDATE_TAGS_HISTORY=true ./main.sh
 
 # update an image
-printf "%s\n" "" "    [TEST] Pushing finch tag to DockerHub" ""
+printf "%s\n" "" "    [TEST] Pushing weaver tag to DockerHub" ""
 curl -XPOST localhost:5000/pavics/weaver/1.13.2-worker
 
 # run updater CLI
-printf "%s\n" "" "    [TEST] Running updater - need to create a PR" ""
+printf "%s\n" "" "    [TEST] Running updater - need to create a PR for [bump_weaver-worker_to_1.13.2-worker]" ""
+rm -f last-diff-result.log
 source test/integration/env.test && ./main.sh
+
+
+### Asserts that diff contains the right thing
+printf "%s\n" "" "    [TEST] ASSERT" ""
+if grep -q 'export FINCH_IMAGE="pavics/weaver:1.13.2-worker"' "last-diff-result.log"; then
+    echo "[INFO] [bump_weaver-worker_to_1.13.2-worker] The commit content looks good"
+    # echo
+    # cat last-diff-result.log
+else
+    echo "ERROR - wrong commit content. Exiting."
+    exit 1
+fi
+
 
 # run updater CLI
 printf "%s\n" "" "    [TEST] Running updater - no PR to create" ""
+rm -f last-diff-result.log
 source test/integration/env.test && ./main.sh
+
+# update an image
+printf "%s\n" "" "    [TEST] Pushing weaver and finch tags to DockerHub" ""
+curl -XPOST localhost:5000/pavics/weaver/1.13.3-worker
+curl -XPOST localhost:5000/birdhouse/finch/version-0.5.4
+
+
+# run updater CLI
+printf "%s\n" "" "    [TEST] Running updater - need to create a PR for [bump_finch_to_version-0.5.4]" ""
+rm -f last-diff-result.log
+source test/integration/env.test && ./main.sh
+
+
+### Asserts that diff contains the right thing
+printf "%s\n" "" "    [TEST] ASSERT" ""
+if grep -q 'export FINCH_IMAGE="birdhouse/finch:version-0.5.4"' "last-diff-result.log"; then
+    echo "[INFO] [bump_finch_to_version-0.5.4] The commit content looks good"
+    # echo
+    # cat last-diff-result.log
+else
+    echo "ERROR - wrong commit content. Exiting."
+    cat last-diff-result.log
+    exit 1
+fi
+
+
+# run updater CLI
+printf "%s\n" "" "    [TEST] Running updater - need to create a PR for [bump_weaver-worker_to_1.13.3-worker]" ""
+rm -f last-diff-result.log
+source test/integration/env.test && ./main.sh
+
+
+### Asserts that diff contains the right thing
+printf "%s\n" "" "    [TEST] ASSERT" ""
+if grep -q 'export FINCH_IMAGE="pavics/weaver:1.13.3-worker"' "last-diff-result.log"; then
+    echo "[INFO] [bump_weaver-worker_to_1.13.3-worker] The commit content looks good"
+    # echo
+    # cat last-diff-result.log
+else
+    echo "ERROR - wrong commit content. Exiting."
+    exit 1
+fi
+
 
 # kill the dummy API
 printf "%s\n" "" "    [TEST] Stopping dummy APIs" ""
@@ -32,14 +90,3 @@ echo "done"
 
 
 
-### Test assertion
-printf "%s\n" "" "    [TEST] RESULTS" ""
-if grep -q 'export FINCH_IMAGE="pavics/weaver:1.13.2-worker"' "last-diff-result.log"; then
-    echo "The commit content looks good"
-    echo
-    cat last-diff-result.log
-    exit 0
-else
-    echo "ERROR - wrong commit content"
-    exit 1
-fi
