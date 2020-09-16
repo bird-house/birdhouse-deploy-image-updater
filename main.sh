cfcbb9f186f64ac52bec99af1213338b598bc2bc
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-09-16 15:40:22 -0400

    debig: creds

--- main.sh
+++ main.sh
@@ -28,8 +28,8 @@ fi
 export REPO_URL=$(cat $CONFIG_FILEPATH | jq '.project.url' | tr -d '\"')
 export PROJECT_NAME=${REPO_URL##*/}
 export ONLY_UPDATE_TAGS_HISTORY=${ONLY_UPDATE_TAGS_HISTORY}
-export GITHUB_USER=$GITHUB_USER | base64 | base64 --decode
-export GITHUB_PASSWORD=$GITHUB_PASSWORD | base64 | base64 --decode
+export GITHUB_USER=$(echo $GITHUB_USER | base64 | base64 --decode)
+export GITHUB_PASSWORD=$(echo $GITHUB_PASSWORD | base64 | base64 --decode)
 export EXIT_BEFORE_PR=$EXIT_BEFORE_PR
 
 echo $GITHUB_USER
