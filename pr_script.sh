1c6ffbce06b9321d9653e89be64371dad7f14756
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-10-28 04:44:40 -0400

    fixed config.prod.json

--- pr_script.sh
+++ pr_script.sh
@@ -49,10 +49,7 @@ cd $WORKING_DIR
 
 if [[ ! -d "$PROJECT_NAME" ]]
 then
-    echo "[INFO] Not existing project directory, cloning"
-    echo $PROJECT_ORG_REPO
-    cat ~/.gitconfig
-    exit
+    echo "[INFO] Not existing project directory, cloning $PROJECT_ORG_REPO"
     hub clone $PROJECT_ORG_REPO
     cd $PROJECT_NAME
 else
